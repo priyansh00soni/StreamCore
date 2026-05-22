b7afb68bb92539289748d12a9a29d02c30ab03df
Priyansh <priyansh00soni@gmail.com>
2026-05-22 21:00:31 +0530

    Add subscription routes and controller functions for managing user subscriptions

---
+++ src/routes/subscription.routes.js
@@ -0,0 +1,11 @@
+import { Router } from "express";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+import { getSubscribedChannels, getUserChannelSubscribers, toggleSubscription } from "../controllers/subscription.contoller.js";
+const router = Router()
+
+router.route('/c').get(verifyJWT,getSubscribedChannels)
+router.route('/c/:channelId').post(verifyJWT,toggleSubscription)
+router.route('/u').get(verifyJWT,getUserChannelSubscribers)
+
+
+export default router
--- src/routes/tweets.routes.js
+++ src/routes/tweets.routes.js
@@ -6,6 +6,6 @@ const router = Router()
 router.route('/').post(verifyJWT,createTweet)
 router.route('/:userId').get(getUserTweets)
 router.route('/:tweetId').patch(verifyJWT,updateTweet)
-router.route('/:tweetId').delete(verifyJWT,deleteTweet)
+                        .delete(verifyJWT,deleteTweet)
 
 export default router
