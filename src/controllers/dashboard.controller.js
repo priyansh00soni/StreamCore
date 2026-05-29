5862bc899701596920a86d20583cd422f547eebf
Priyansh <priyansh00soni@gmail.com>
2026-05-29 11:40:43 +0530

    Add user and video model imports; update channel videos response structure

--- src/controllers/dashboard.controller.js
+++ src/controllers/dashboard.controller.js
@@ -2,6 +2,9 @@ import mongoose from "mongoose";
 import asyncHandler from "../utils/asyncHandler.js";
 import ApiError from "../utils/ApiError.js";
 import ApiResponse from "../utils/ApiResponse.js";
+import {User} from  '../models/user.model.js'
+import {Video} from  '../models/videos.model.js'
+
 
 const getChannelStats = asyncHandler(async(req,res)=>{
     const stats = await User.aggregate([
@@ -103,8 +106,8 @@ const getChannelVideos = asyncHandler(async (req, res) => {
     
     const videos = await Video.find({ owner: req.user._id }).sort({createdAt: -1, })
 
-    if(!videos) throw new ApiError(404,"Videos not found.") 
-    return res.status(200).json(new ApiResponse(200,videos[0].videos,"Videos fetched successfully."))
+    if(!videos || videos.length ===0) throw new ApiError(404,"Videos not found.") 
+    return res.status(200).json(new ApiResponse(200,videos,"Videos fetched successfully."))
 })
 
 export {getChannelStats, getChannelVideos}
--- src/controllers/healthcheck.controller.js
+++ src/controllers/healthcheck.controller.js
@@ -1,11 +1,10 @@
-import {ApiError} from "../utils/ApiError.js"
-import {ApiResponse} from "../utils/ApiResponse.js"
-import {asyncHandler} from "../utils/asyncHandler.js"
+import ApiResponse from "../utils/ApiResponse.js"
+import asyncHandler from "../utils/asyncHandler.js"
 
 
 const healthcheck = asyncHandler(async (req, res) => {
     return res.status(200).json(new ApiResponse(200,{status:"OK"},"Service is running smoothly."))
-})
+}) 
 
-export {healthcheck}
+export {healthcheck} 
     
